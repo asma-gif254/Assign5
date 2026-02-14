cd4433dd21f68a06f298d34542543947003875dc
asma-gif254 <am9182948@gmail.com>
2026-02-15 00:39:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Assign5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //Console.WriteLine("Enter A Day Number (1-7) : ");
+            //int input = int.Parse(Console.ReadLine());
+            //DayOfWeek day = (DayOfWeek)input;
+            //switch (day)
+            //{
+            //    case DayOfWeek.Saturday:
+            //        Console.WriteLine("Saturday");
+            //        Console.WriteLine("It's the Weekend");
+            //        break;
+            //    case DayOfWeek.Sunday:
+            //        Console.WriteLine("Sunday");
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //    case DayOfWeek.Monday:
+            //        Console.WriteLine("Monday");
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //    case DayOfWeek.Tuesday:
+            //        Console.WriteLine("Tuesday");
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //    case DayOfWeek.Wednesday:
+            //        Console.WriteLine("Wednesday");
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //    case DayOfWeek.Thursday:
+            //        Console.WriteLine("Thursday");
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //    case DayOfWeek.Friday:
+            //        Console.WriteLine("Friday");
+            //        Console.WriteLine("It's the Weekend");
+            //        break;
+            //            default:
+            //                Console.WriteLine("Enter valid work day");
+            //                break;
+            //        }
+
+
+        }
+    }
+}
---
+++ enum/DayOfWeek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign5.internalenum
+{
+    enum DayOfWeek
+{
+    Saturday =1,
+        Sunday ,
+       Monday ,
+        Tuesday ,
+        Wednesday ,
+        Thursday ,
+        Friday ,
+
+}
+}
+
